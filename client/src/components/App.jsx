dd96480548130acd3c9f7f1b6644ce51ee88da48
Chris Bell <cb.camellia@gmail.com>
2020-11-24 21:24:55 -0800

    Use centralized state

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,18 +1,46 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useReducer } from 'react';
 import ReactDOM from 'react-dom';
-import Controller from './Controller.js';
+import { GameContext } from './GameContext.jsx';
+import Game from './Game.jsx';
 import Ship from './Ship.jsx';
 
+//
+const initialGameState = {
+  objects: {
+    player: null,
+    asteroids: [],
+  },
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'asteroid':
+      return {
+        ...state,
+        objects: {
+          ...state.objects,
+          asteroids: [...state.asteroids, action.asteroid],
+        },
+      };
+    case 'player':
+      return {
+        ...state,
+        objects: {
+          ...state.objects,
+          player: action.player,
+        },
+      };
+  }
+};
+
 const App = () => {
-  const [PlayerShip, PlayerShipRender] = new Ship();
-  const Input = new Controller({
-    mousemove: [PlayerShip.rotateToCursor.bind(PlayerShip)],
-    mousedown: [],
-    mouseup: [],
-    keypress: [],
-    keyup: [],
-  });
-  return PlayerShipRender;
+  const [gameState, dispatch] = useReducer(reducer, initialGameState);
+
+  return (
+    <GameContext.Provider value={{ gameState, dispatch }}>
+      <Game />
+    </GameContext.Provider>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById('app'));
--- client/src/components/Game.jsx
+++ client/src/components/Game.jsx
@@ -1,16 +1,14 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
+import { GameContext } from './GameContext.jsx';
 import Ship from './Ship.jsx';
-function Game() {
-  this.objects = [new Ship()];
-  return this.objects;
-}
 
-Game.prototype.render = function () {
-  return this.objects;
-};
-
-Game.prototype.addObject = function (obj) {
-  this.objects.push(obj);
+const Game = () => {
+  const { gameState, dispatch } = useContext(GameContext);
+  const PlayerShip = new Ship();
+  useEffect(() => {
+    dispatch({ type: 'player', player: PlayerShip });
+  }, []);
+  return PlayerShip;
 };
 
 export default Game;
