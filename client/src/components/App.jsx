aed66403de89cd523648d25e44c93da1dd30e2b7
Chris Bell <cb.camellia@gmail.com>
2020-11-28 16:27:41 -0500

    Add container for playfield

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,30 @@
 import React, { useState, useReducer } from 'react';
 import ReactDOM from 'react-dom';
+import $ from 'jquery';
+import styled from 'styled-components';
 import { GameContext } from './GameContext.jsx';
 import Game from './Game.jsx';
 import Ship from './Ship.jsx';
 
+const Window = styled.div`
+  position: absolute;
+  display: block;
+  width: ${(props) => props.size.width}px;
+  height: ${(props) => props.size.height}px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border: dashed red;
+  overflow-x: hidden;
+  overflow-y: hidden;
+`;
+
 const initialGameState = {
-  screenWidth: 1920,
-  screenHeight: 1080,
+  screen: {
+    width: Math.round($(window).width() * 0.95),
+    height: Math.round($(window).height() * 0.95),
+  },
+  updateSpeed: 10,
   level: 1,
   lives: 3,
   objects: {
@@ -28,9 +46,12 @@ const reducer = (state, action) => {
 
 const App = () => {
   const [gameState, dispatch] = useReducer(reducer, initialGameState);
+  console.log(JSON.stringify(gameState.screen));
   return (
     <GameContext.Provider value={{ gameState, dispatch }}>
-      <Game />
+      <Window size={gameState.screen}>
+        <Game />
+      </Window>
     </GameContext.Provider>
   );
 };
